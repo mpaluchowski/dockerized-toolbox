85f78b1738799553861458c6b53acccc9d5466f6
Michal Paluchowski <Michal.Paluchowski@stepstone.com>
2016-09-21 17:00:50 +0200

    Add ability to build some containers

--- build-all.sh
+++ build-all.sh
@@ -3,6 +3,8 @@
 # Default prefix is the current directory name
 export DOCKER_IMAGE_PREFIX=${PWD##/*/}
 
+IMAGES_TO_BUILD=()
+
 while [[ $# -ge 1 ]]; do
     key="$1"
     case $key in
@@ -12,16 +14,33 @@ while [[ $# -ge 1 ]]; do
         shift
         ;;
         *)
-            # unknown option
+        # Name of container to build
+        IMAGES_TO_BUILD=(${IMAGES_TO_BUILD[@]} ${1%/})
         ;;
     esac
     shift
 done
 
-find -maxdepth 1 \
-	-type d \
-	-not -path . \
-	-not -path '*/\.*' \
-	-exec bash -c \
-	'docker build -t $DOCKER_IMAGE_PREFIX/${1##.*/} $1' \
-	-- {} \;
+build-all() {
+	echo "Building all images"
+	find -maxdepth 1 \
+		-type d \
+		-not -path . \
+		-not -path '*/\.*' \
+		-exec bash -c \
+		'docker build -t $DOCKER_IMAGE_PREFIX/${1##.*/} $1' \
+		-- {} \;
+}
+
+build-some() {
+	for container in ${IMAGES_TO_BUILD[@]}; do
+		echo "Building $DOCKER_IMAGE_PREFIX/$container"
+		docker build -t $DOCKER_IMAGE_PREFIX/$container $container
+	done
+}
+
+if [ ${#IMAGES_TO_BUILD[@]} -gt 0 ]; then
+	build-some
+else
+	build-all
+fi
